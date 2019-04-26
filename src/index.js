29fc930d13f2620c5240ca447a7dbf747181cdb0
Dirt Deodara <thecrowleaps@gmail.com>
2019-04-26 14:29:23 -0700

    cleaning up my code so I can get oriented in my program a bit easier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-//import function
+
 import randomWholeNum from './wholeNumbers.js'
 
-//referenc DOM elemets
-let  resultMessage = document
+
+const rollSixButton = document.getElementById(six-button);
+const rollTwentyButton = document.getElementById(twenty-button);
+let  resultMessage = document.getElementById(result-message);
+
 //initiallize some stuff
-let sixSidedResult = '';
-let twentySidedResult = '';
-//add event listener
-roleDie.addEventListener('click', () => {
-    randomWholeNum()
-    
+//let sixSidedResult = '';
+//let twentySidedResult = '';
+
+
+rollSixButton.addEventListener('click', () => {
+    resultMessage.textContent = randomWholeNum(7);
 });
+
+rollTwentyButton.addEventListener('click', () => {
+    resultMessage.textContent = randomWholeNum(21);
+});
+
+
 //show message
